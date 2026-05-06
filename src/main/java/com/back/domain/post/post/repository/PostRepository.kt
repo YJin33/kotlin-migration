9680f6c819713201e7caaec1281ddc07e34d2f69
Yujin <ujin3261@gmail.com>
2026-05-06 20:09:30 +0900

    31-post,membet repository

--- src/main/java/com/back/domain/post/post/repository/PostRepository.kt
+++ src/main/java/com/back/domain/post/post/repository/PostRepository.kt
@@ -3,5 +3,5 @@ package com.back.domain.post.post.repository;
 import com.back.domain.post.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Integer> {
+interface PostRepository : JpaRepository<Post, Int> {
 }
